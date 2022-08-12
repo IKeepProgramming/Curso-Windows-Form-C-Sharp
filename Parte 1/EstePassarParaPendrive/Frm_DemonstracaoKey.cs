76464e0499fc994c4cdd98409e402a916d538849
IKeepProgramming <amaraz228@gmail.com>
2022-08-12 12:42:43 -0300

    Passagem da Primeira parte do curso de Windows Forms para a pasta 1

---
+++ Parte 1/EstePassarParaPendrive/Frm_DemonstracaoKey.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_DemonstracaoKey : Form
+    {
+        public Frm_DemonstracaoKey()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Txt_Msg.Text = "";
+            Txt_Input.Text = "";
+            Lbl_Upper.Text = "";
+            Lbl_Lower.Text = "";
+        }
+
+        private void Lbl_Minus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txt_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            Txt_Msg.AppendText("\r\n" + "Pressionei a Tecla: " + e.KeyCode + "\r\n"); // -> AppendText cria uma linha de texto em UTF-8
+            Txt_Msg.AppendText("\t" + "Código da Tecla: " + ((int)e.KeyCode) + "\r\n"); // -> \t == tab em c#
+            Txt_Msg.AppendText("\t" + "Nome da Tecla: " + e.KeyData + "\r\n");
+            Lbl_Lower.Text = e.KeyCode.ToString().ToLower();
+            Lbl_Upper.Text = e.KeyCode.ToString().ToUpper();
+        }
+    }
+}
